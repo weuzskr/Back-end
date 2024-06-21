d79818570e2d2c625612ca0a843b2c4ac1bf4b03
Ousmane <119879369+weuzskr@users.noreply.github.com>
2024-06-21 17:01:33 +0000

    Delete Pcc_Angular18-main/Pcc_Angular18-main/Angular directory

--- Pcc_Angular18-main/Pcc_Angular18-main/Angular/src/app/models/ligne.ts
+++
@@ -1,7 +0,0 @@
-export class Ligne {
-    id: number = 0;
-    numLigne: number = 0;
-    depart: string = "";
-    arrivee: string = "";
-    sections: any[] = [];
-}
--- Pcc_Angular18-main/Pcc_Angular18-main/Angular/src/app/services/apiUrl.ts
+++
@@ -1,2 +0,0 @@
-export let url="http://127.0.0.1:8000/api";
-// export let urlImage = "http://127.0.0.1:8000/storage/"
